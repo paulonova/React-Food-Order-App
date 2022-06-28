cf06b7a4fd8616fd68da15b192eadfb8829c19af
Paulo Nova <paulovilanova@MacBook-Pro-som-tillhor-Paulo.local>
2022-06-28 10:16:58 +0200

    Createing Layout and Meals

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # React-Food-Order-App
+
 Own practicing project
+
+### Using Css Modules
+
+Link: https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <heade className={classes.header}>
+        <h1>React Food Order</h1>
+        <HeaderCartButton />
+      </heade>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt='delicious food!' />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
